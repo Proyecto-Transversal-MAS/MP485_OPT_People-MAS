0412f1445ae855b1304b25d5477264dfa72e90c1
aniol <aniolnicolauroig@gmail.com>
2025-04-29 12:48:58 +0200

    issue #2 Constant created at Constnt class and applied into ControllerImplememtation file.

--- src/main/java/controller/ControllerImplementation.java
+++ src/main/java/controller/ControllerImplementation.java
@@ -115,15 +115,7 @@ public class ControllerImplementation implements IController, ActionListener {
         }
     }
     
-    
-    HASHMAP_SELECTED = "HashMap";
-    public static final String FILE_SELECTED = "File";
-    public static final String FILE_SERIALIZATION_SELECTED = "File (Serialization)";
-    public static final String SQL_DATABASE_SELECTED = "SQL - Database";
-    public static final String JPA_DATABASE_SELECTED = "JPA - Database";
-    
-    
-    
+
     
 
     private void handleDataStorageSelection() {
